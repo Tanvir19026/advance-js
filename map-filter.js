7b4df2e3efbf86682e4c448a643f6e7c6f837401
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-02-02 19:38:39 +0600

    map-filter-find

---
+++ map-filter.js
@@ -0,0 +1,51 @@
+
+// square ber korar type
+const numbers=[1,2,3,4,5]
+const square=[];
+//ager niom
+// for(let i=0;i<numbers.length;i++)
+// {
+//    const element=numbers[i];
+//    const result=element*element;
+//    square.push(result);
+// }
+
+// console.log(square);
+
+// use function
+// function double(element)
+// {
+//   return element*element;
+// }
+
+
+// ***map
+//new map use
+// const result=numbers.map(function(element){
+//   return element*element;
+// })
+// console.log(result);
+
+// map e function dokale 3 ta perameter pawa jabe
+// 1.element,2.index,3.array||
+// ***array return kore
+
+
+// use array function
+
+const result=numbers.map(x => x*x)
+console.log(result);
+
+// ***filter
+//filter use kore olpo kico element select korar jonne and sorto full fill korte use hoy
+// ***array return kore
+
+const bigger=numbers.filter(x => x>2)   //2 er theke boro gola show korbe
+console.log(bigger);
+
+// ***find
+const smaller=numbers.find(x => x>2)   //2 er theke boro gola show korbe
+console.log(smaller);
+
+
+
