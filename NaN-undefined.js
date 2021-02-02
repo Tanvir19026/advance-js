313a595df6ecd9143fe2f30e7905890c327c515a
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-02-02 18:49:22 +0600

    null-undefined

---
+++ NaN-undefined.js
@@ -0,0 +1,34 @@
+value declare na korle:::
+
+// let pakhi;
+// console.log(pakhi);
+
+return na korle::
+// function add(a,b)
+// {
+//   console.log(a+b);
+//   return 
+// }
+ 
+peremeter pass na korle
+
+// function add(a,b)
+// {
+//   console.log(a,b);
+// }
+// const result=add(10);
+// console.log(result);
+
+object a property na thkle 
+
+// const premik={name:"rafi",phone:015654};
+// console.log(premik.gfname);
+
+undefined set korle
+
+// const fun=undefined;
+// console.log(fun);
+
+
+null mane 0 ;
+eta set kora jay
--- true-false.js
+++ true-false.js
@@ -1,2 +1,13 @@
-false value:
-0
+// false value:
+// 0
+// ''
+// NaN
+// undefined
+// null
+// false
+
+// other value is true
+// ' '
+// '0'
+// etc
+
