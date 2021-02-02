3f08d6d6ae27f73267ed197cecd482e4060d8cef
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-02-02 19:39:32 +0600

    ok

--- map-filter.js
+++ map-filter.js
@@ -2,50 +2,51 @@
 // square ber korar type
 const numbers=[1,2,3,4,5]
 const square=[];
-//ager niom
-// for(let i=0;i<numbers.length;i++)
-// {
-//    const element=numbers[i];
-//    const result=element*element;
-//    square.push(result);
-// }
+// ager niom
+for(let i=0;i<numbers.length;i++)
+{
+   const element=numbers[i];
+   const result=element*element;
+   square.push(result);
+}
 
-// console.log(square);
+console.log(square);
 
 // use function
-// function double(element)
-// {
-//   return element*element;
-// }
+function double(element)
+{
+  return element*element;
+}
 
 
 // ***map
-//new map use
-// const result=numbers.map(function(element){
-//   return element*element;
-// })
-// console.log(result);
+// new map use
+const result=numbers.map(function(element){
+  return element*element;
+})
+console.log(result);
 
 // map e function dokale 3 ta perameter pawa jabe
 // 1.element,2.index,3.array||
 // ***array return kore
 
 
-// use array function
+// use arrow function
 
 const result=numbers.map(x => x*x)
 console.log(result);
 
 // ***filter
-//filter use kore olpo kico element select korar jonne and sorto full fill korte use hoy
+// filter use kore olpo kico element select korar jonne and sorto full fill korte use hoy
 // ***array return kore
 
 const bigger=numbers.filter(x => x>2)   //2 er theke boro gola show korbe
 console.log(bigger);
 
-// ***find
+// ***find  shodho matro element dey
 const smaller=numbers.find(x => x>2)   //2 er theke boro gola show korbe
 console.log(smaller);
+
 
 
 
