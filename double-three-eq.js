2819f458158e3caff8fc7841d1f3c8fd06ab4eda
Tanvir19026 <tanvirrafi1999@gmail.com>
2021-02-02 19:04:45 +0600

    == and ===

---
+++ double-three-eq.js
@@ -0,0 +1,49 @@
+
+
+
+     == mane shodho value check korbe
+     === mane value and type check korbe
+     
+    const rafi=5;
+    const meem='5';
+    if(rafi==meem)
+    {
+      console.log("value match");
+    }
+    else{
+      console.log("value unmatch");
+    }
+
+    ans is value match cuze (==) shodho value match korbe
+
+    const rafi=5;
+    const meem='5';
+    if(rafi===meem)
+    {
+      console.log("value match");
+    }
+    else{
+      console.log("value unmatch");
+    }
+ans is value unmatch cuze (===)  value and data type match korbe
+
+    const rafi=0;
+    const meem=false;
+    if(rafi==meem)
+    {
+      console.log("value match");
+    }
+    else{
+      console.log("value unmatch");
+    }
+
+
+    const rafi=1;
+    const meem=true;
+    if(rafi===meem)
+    {
+      console.log("value match");
+    }
+    else{
+      console.log("value unmatch");
+    }
